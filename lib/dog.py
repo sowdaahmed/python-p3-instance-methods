ed688a40c25954c3a269a75566c405b42113f905
Sowda Ahmed <ayanmoha4949@gmail.com>
2025-06-17 12:15:56 -0700

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,11 @@
+
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    # bark instance method
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    # sit instance method
+    def sit(self):
+        print("The dog is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,14 @@
+
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+class Person:
+    def __init__(self, name="Guido", age=0):  # Provide defaults so Person() works
+        self.name = name
+        self.age = age
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
